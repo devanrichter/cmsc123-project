4e1c419f8390c8927814d5345cf5ec98ccd1edb9
devanrichter <devanrichter@uchicago.edu>
2018-05-28 21:19:22 -0500

    Create CSV
    File that takes lastfm_tags database and tracks_metadata database and creates uniform output of 3 field tuple - song name, artist, tag - for all tags of all songs in last fm dataset

---
+++ create_csv.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import sqlite3
+import time
+
+
+dbfile = "track_metadata.db"
+conn = sqlite3.connect(dbfile)
+c = conn.cursor()
+c.execute("ATTACH DATABASE 'lastfm_tags.db' AS 'lastfm_tags'")
+
+with open("tracks_with_tag.txt") as f:
+    for line in f:
+        clean_line = line.strip()
+        track_id = clean_line
+        c.execute("SELECT songs.title, songs.artist_name, lastfm_tags.tags.tag\
+        FROM lastfm_tags.tid_tag, lastfm_tags.tids, lastfm_tags.tags, songs\
+        WHERE lastfm_tags.tags.ROWID=lastfm_tags.tid_tag.tag\
+        AND lastfm_tags.tid_tag.tid=lastfm_tags.tids.ROWID\
+        AND lastfm_tags.tids.tid='%s'\
+        AND songs.track_id='%s'" % (track_id,track_id))
+        data = c.fetchall()
+        for item in data:
+            print(item)
+conn.close()
